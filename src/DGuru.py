87b74899376a5b1f0abf1c0f45e742f95825a081
DuskyElf <clayedison75@gmail.com>
2021-12-30 18:48:38 +0530

    Implemented Parser

--- src/DGuru.py
+++ src/DGuru.py
@@ -1,6 +1,31 @@
 ### DIGITS ###
 DIGITS = '0123456789'
 
+### Super String ###
+def super_string(text, pos_start, pos_end):
+    result = ''
+    
+    # Calculate indices
+    idx_start = max(text.rfind('\n', 0, pos_start.index), 0)
+    idx_end = text.rfind('\n', idx_start + 1)
+    if idx_end < 0: idx_end = len(text)
+    
+    # Gnerate lines
+    line_count = pos_end.linumber - pos_start.linumber + 1
+    for i in range(line_count):
+        line = text[idx_start:idx_end]
+        col_start = pos_start.conumber if i == 0 else 0
+        col_end = pos_end.conumber if i == line_count - 1 else len(line) - 1
+        
+        result += line + '\n'
+        result += ' ' * col_start + '^' * (col_end - col_start)
+        
+        idx_start = idx_end
+        idx_end = text.find('\n', idx_start + 1)
+        if idx_end < 0: idx_end = len(text)
+    
+    return result.replace('\t', '')
+
 ### ERRORS ###
 class Error:
     def __init__(self, pos_start, pos_end, error_name, details):
@@ -12,11 +37,16 @@ class Error:
     def as_string(self):
         result = f'{self.error_name}: {self.details}\n'
         result += f'File {self.pos_start.fname}, line {self.pos_start.linumber + 1}'
+        result += '\n\n' + super_string(self.pos_start.ftext, self.pos_start, self.pos_end)
         return result
 
 class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details):
-        super().__init__(pos_start, pos_end, 'Illegal Character', details)
+        super().__init__(pos_start, pos_end, "IllegalCharError", details)
+
+class InvalidSyntaxError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
 
 ### POSITION ###
 class Position:
@@ -27,7 +57,7 @@ class Position:
         self.fname = fname
         self.ftext = ftext
     
-    def increment(self, current_char):
+    def increment(self, current_char=None):
         self.index += 1
         self.conumber += 1
         
@@ -50,11 +80,20 @@ TT_MUL      = 'MUL'
 TT_DIV      = 'DIV'
 TT_LPAREN   = 'LPAREN'
 TT_RPAREN   = 'RPAREN'
+TT_EOF      = 'EOF'
 
 class Token:
-    def __init__(self, type_, value=None):
+    def __init__(self, type_, value=None, pos_start=None, pos_end=None):
         self.type = type_
         self.value = value
+        
+        if pos_start:
+            self.pos_start = pos_start.copy()
+            self.pos_end = pos_start.copy()
+            self.pos_end.increment()
+        
+        if pos_end:
+            self.pos_end = pos_end.copy()
     
     def __repr__(self):
         if self.value: return f"{self.type}:{self.value}"
@@ -82,22 +121,22 @@ class Lexer:
             elif self.current_char in DIGITS:
                 tokens.append(self.define_number())
             elif self.current_char == '+':
-                tokens.append(Token(TT_PLUS))
+                tokens.append(Token(TT_PLUS, pos_start=self.pos))
                 self.increment()
             elif self.current_char == '-':
-                tokens.append(Token(TT_MINUS))
+                tokens.append(Token(TT_MINUS, pos_start=self.pos))
                 self.increment()
             elif self.current_char == '*':
-                tokens.append(Token(TT_MUL))
+                tokens.append(Token(TT_MUL, pos_start=self.pos))
                 self.increment()
             elif self.current_char == '/':
-                tokens.append(Token(TT_DIV))
+                tokens.append(Token(TT_DIV, pos_start=self.pos))
                 self.increment()
             elif self.current_char == '(':
-                tokens.append(Token(TT_LPAREN))
+                tokens.append(Token(TT_LPAREN, pos_start=self.pos))
                 self.increment()
             elif self.current_char == ')':
-                tokens.append(Token(TT_RPAREN))
+                tokens.append(Token(TT_RPAREN, pos_start=self.pos))
                 self.increment()
             else:
                 pos_start = self.pos.copy()
@@ -105,11 +144,13 @@ class Lexer:
                 self.increment()
                 return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
         
+        tokens.append(Token(TT_EOF, pos_start=self.pos))
         return tokens, None
     
     def define_number(self):
         num_str = ''
         period_count = 0
+        pos_start = self.pos.copy()
         
         while self.current_char != None and self.current_char in DIGITS + '.':
             if self.current_char == '.':
@@ -121,13 +162,108 @@ class Lexer:
             self.increment()
             
         if period_count == 0:
-            return Token(TT_INT, int(num_str))
+            return Token(TT_INT, int(num_str), pos_start, self.pos)
         else:
-            return Token(TT_FLOAT, float(num_str))
+            return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
+
+### NODES ###
+
+class NumberNode:
+    def __init__(self, token):
+        self.token = token
+    
+    def __repr__(self):
+        return f"{self.token}"
+
+class BinOpNode:
+    def __init__(self, left_node, op_token, right_node):
+        self.left_node = left_node
+        self.op_token = op_token
+        self.right_node = right_node
+    
+    def __repr__(self):
+        return f"({self.left_node}, {self.op_token}, {self.right_node})"
+
+### PARSE RESULT
+class ParseResult:
+    def __init__(self):
+        self.error = None
+        self.node = None
+    
+    def register(self, res):
+        if isinstance(res, ParseResult):
+            if res.error: self.error = res.error
+            return res.node
+        
+        return res
+    
+    def success(self, node):
+        self.node = node
+        return self
+    
+    def failure(self, error):
+        self.error = error
+        return self
+
+### PARSER ###
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.token_index = -1
+        self.increment()
+    
+    def increment(self):
+        self.token_index += 1
+        if self.token_index < len(self.tokens):
+            self.current_token = self.tokens[self.token_index]
+        return self.current_token
+    
+    def parser(self):
+        res = self.expr()
+        if not res.error and self.current_token.type != TT_EOF:
+            return res.failure()
+        return res
+
+    def factor(self):
+        res = ParseResult()
+        
+        tok = self.current_token
+        if tok.type in (TT_INT, TT_FLOAT):
+            res.register(self.increment())
+            return res.success(NumberNode(tok))
+        
+        return res.failure(InvalidSyntaxError(
+            tok.pos_start, tok.pos_end,
+            "Expected int or float"
+        ))
+    
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+    
+    def expr(self):
+        return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+    
+    def bin_op(self, func, ops):
+        res = ParseResult()
+        left = res.register(func())
+        if res.error: return res
+        
+        while self.current_token.type in ops:
+            op_token = self.current_token
+            res.register(self.increment())
+            right = res.register(func())
+            if res.error: return res
+            left = BinOpNode(left, op_token, right)
+            
+        return res.success(left)
 
 ### RUN ###
 def run(fname, text):
     lexer = Lexer(fname, text)
     tokens, error = lexer.make_tokens()
+    if error: return None, Error
     
-    return tokens, error
+    parser = Parser(tokens)
+    abstractSyntaxTree = parser.parser()
+    
+    return abstractSyntaxTree.node, abstractSyntaxTree.error
